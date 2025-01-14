ac3fe066524f574b8918297b35968ecb74621e8a
Alexandr Goremykin <AGoremykin@dar.io>
2025-01-14 12:41:20 +0500

    fix: change test domain to our dev ip

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ var config = {
 
     hosts: {
         // XMPP domain.
-        domain: '135.181.224.218:8443',
+        domain: '135.181.224.218',
 
         // When using authentication, domain for guest users.
         // anonymousdomain: 'guest.example.com',
@@ -42,11 +42,11 @@ var config = {
         // focus: 'focus.jitsi-meet.example.com',
 
         // XMPP MUC domain. FIXME: use XEP-0030 to discover it.
-        muc: 'conference.' + subdomain + '135.181.224.218:8443',
+        muc: 'conference.' + subdomain + '135.181.224.218',
     },
 
     // BOSH URL. FIXME: use XEP-0156 to discover it.
-    bosh: 'https://135.181.224.218:8443/' + subdir + 'http-bind',
+    bosh: '135.181.224.218/' + subdir + 'http-bind',
 
     // Websocket URL (XMPP)
     // websocket: 'wss://jitsi-meet.example.com/' + subdir + 'xmpp-websocket',
